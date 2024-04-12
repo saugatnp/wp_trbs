427e4e6be70dcc22a6b2eab8c6682a0b5fc8031c
saugatnp <saugatnp11@gmail.com>
2024-04-12 11:40:01 +0545

    message sent from chat page functionality added

--- wp-content/plugins/chat/index.php
+++ wp-content/plugins/chat/index.php
@@ -99,6 +99,8 @@ function sa_chat_list_callback()
             overflow-y: auto;
             display: flex;
             flex-direction: column;
+            max-height: 600;
+            overflow-y: scroll;
         }
 
         .message {
@@ -127,11 +129,14 @@ function sa_chat_list_callback()
             /* Align sender's message to the left */
         }
 
-        .receiver .message-content {
+         .receiver .message-content {
             background-color: #7fd2ff;
             /* Example color for receiver's message */
             align-self: flex-end;
             /* Align receiver's message to the right */
+        }
+        .receiver span {
+            align-self: flex-end;
         }
 
         .timestamp {
@@ -189,6 +194,8 @@ function sa_chat_list_callback()
             font-size: 20px;
         }
     </style>
+
+
     <?php
     global $wpdb;
     $table_name = $wpdb->prefix . 'conversation';
@@ -196,13 +203,64 @@ function sa_chat_list_callback()
     $message_table = $wpdb->prefix . 'messages';
     $userId = get_current_user_id();
     $result =  $wpdb->get_results(
-        "SELECT a.* ,b.user_nicename from $table_name a join $user_table b on ( case when a.user_one = $userId then a.user_two else a.user_one end ) = b.ID;"
+        "SELECT a.* ,b.user_nicename , b.user_nicename from $table_name a join $user_table b on ( case when a.user_one = $userId then a.user_two else a.user_one end ) = b.ID;"
     );
+
+
+    // function getUserList($wpdb, $table_name, $user_table, $userId)
+    // {
+    //     $result = $wpdb->get_row(
+    //         $wpdb->prepare(
+    //             "SELECT a.* ,b.user_nicename from $table_name a join $user_table b on ( case when a.user_one = $userId then a.user_two else a.user_one end ) = b.ID;"
+    //         )
+    //     );
+    //     return $result;
+    // }
+    // function getUserChat($conversation_id){
+    //     global $wpdb;
+    //     $table_name = $wpdb->prefix . 'conversation';
+    //     $message_table = $wpdb->prefix . 'messages';
+    //     $result =  $wpdb->get_results(
+    //         $wpdb->get_results("SELECT a.message , a.sender_id , a.sent_at FROM $message_table a join $table_name b on a.conversation_id = b.conversation_id where a.conversation_id = $conversation_id order by a.sent_at asc;")
+    //     );
+    //     return $result;
+    // }
+
+    global $conversation_id;
+    global $chatWithUserName;
     if (isset($_REQUEST['namebtn'])) {
+        global $chatWithUserName;
         $conversation_id = $_REQUEST['conversation_id'];
+        $chatWithUserName = $_REQUEST['user_nicename'];
+        $chats = $wpdb->get_results("SELECT a.message , a.sender_id , a.sent_at FROM $message_table a join $table_name b on a.conversation_id = b.conversation_id where a.conversation_id = $conversation_id order by a.sent_at asc;
+        ");
+        
+    }
+
+
+
+    if (isset($_REQUEST['sendmessage'])) {
+        $message = $_REQUEST['message'];
+        $conversation_id = $_REQUEST['conversation_id'];
+        $wpdb->insert(
+            $message_table,
+            array(
+                'conversation_id' => $conversation_id,
+                'sender_id' => $userId,
+                'message' => $message
+            )
+        );
         $chats = $wpdb->get_results("SELECT a.message , a.sender_id , a.sent_at FROM $message_table a join $table_name b on a.conversation_id = b.conversation_id where a.conversation_id = $conversation_id order by a.sent_at asc;
         ");
     }
+
+    
+
+
+
+
+
+
     ?>
     <div class="container">
         <div class="sidebar">
@@ -213,6 +271,7 @@ function sa_chat_list_callback()
                 <?php
                 foreach ($result as $row) {
                     echo "<form method='post'>";
+                    echo "<input type='hidden' name='user_nicename' value='$row->user_nicename'>";
                     echo "<input type='hidden' name='conversation_id' value='$row->conversation_id'>";
                     echo "<li><button class='name-btn' name='namebtn' type='submit' value='namebtn'>" . $row->user_nicename . "</button></li>";
                     echo "</form>";
@@ -221,23 +280,35 @@ function sa_chat_list_callback()
             </ul>
         </div>
         <div class="conversation-container">
-            <center><b>Messages&nbsp;</b></center>
+            <center><b>Messages&nbsp;(<?php echo $chatWithUserName ?>)</b></center>
+            <?php
+            if (isset($_REQUEST['namebtn']) || isset($_REQUEST['sendmessage'])) {
+            ?>
+                <div class="conversation" id="conversation">
+                    <?php
 
-            <div class="conversation" id="conversation">
-                <?php
-                if (isset($_REQUEST['namebtn'])) {
                     foreach ($chats as $row) {
-                        echo "<div class='message'><span>$row->message</span><span class='timestamp'>$row->sent_at</span></div>";
+                        if ($row->sender_id == $userId) {
+                            echo "<div class='message receiver'><span class='sender'>You</span><span class='message-content'>$row->message</span><span class='timestamp'>$row->sent_at</span></div>";
+                        } else
+                            echo "<div class='message'><span>$row->message</span><span class='timestamp'>$row->sent_at</span></div>";
                     }
-                }
-                ?>
 
-                <!-- Conversation will be populated here -->
-            </div>
-            <div class="input-container">
-                <textarea id="message-input" placeholder="Type your message..."></textarea>
-                <button id="send-btn">Send</button>
-            </div>
+                    ?>
+
+                    <!-- Conversation will be populated here -->
+                </div>
+
+                <div class="input-container">
+                    <form method="post">
+                        <input type="hidden" name="conversation_id" value="<?php echo $conversation_id; ?>">
+                        <textarea id="message-input" name="message" placeholder="Type your message..."></textarea>
+                        <button id="send-btn" name="sendmessage">Send</button>
+                    </form>
+                </div>
+            <?php
+            }
+            ?>
         </div>
     </div>
 <?php
